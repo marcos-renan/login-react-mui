c7539a29d5c8d7553d7ba93d0cdc49caf569f9f7
Marcos <devmarcos7@gmail.com>
2024-11-20 16:27:13 -0300

    estilizando ui

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,25 +1,28 @@
-import { Button, TextField, Typography } from "@mui/material"
+import { Box, Button, TextField, Typography } from "@mui/material"
 
 
 const Login = () => {
 
   return (
-    <>
+    <Box
+      bgcolor={"#7159c1"}
+      display={"flex"}
+      flexDirection={"column"}
+      minHeight={"100vh"}
+    >
       <Typography variant="h1" align="center">
         Sign In
       </Typography>
-      <TextField
-        type="email"
-        label="E-mail"
-        variant="outlined"
-      />
-      <TextField
-        type="password"
-        label="Senha"
-        variant="outlined"
-      />
+      <Box
+        bgcolor={"#d7d2d7"}
+        display={"flex"}
+        flexDirection={"column"}
+      >
+        <TextField type="email" label="E-mail" variant="outlined" />
+        <TextField type="password" label="Senha" variant="outlined" />
+      </Box>
       <Button variant="contained">Entrar</Button>
-    </>
+    </Box>
   );
 }
 
