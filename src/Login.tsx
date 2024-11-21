68b4939a8ec32584f94c50882b7790aff9f86bad
Marcos <devmarcos7@gmail.com>
2024-11-21 15:19:39 -0300

    construindo controle de formulario

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,27 +1,35 @@
-import { Box, Button, TextField, Typography } from "@mui/material"
+import { Box, Button, FormControl, TextField, Typography } from "@mui/material"
 
 
 const Login = () => {
 
   return (
+    
     <Box
       bgcolor={"#7159c1"}
       display={"flex"}
       flexDirection={"column"}
       minHeight={"100vh"}
     >
-      <Typography variant="h1" align="center">
-        Sign In
-      </Typography>
+
+      <Typography align="center">Acessar a conta</Typography>
+
       <Box
+        component={"form"}
         bgcolor={"#d7d2d7"}
         display={"flex"}
         flexDirection={"column"}
       >
-        <TextField type="email" label="E-mail" variant="outlined" />
+        <FormControl>
+          <TextField type="email" label="E-mail" variant="outlined" />
+        </FormControl>
+
         <TextField type="password" label="Senha" variant="outlined" />
+
       </Box>
+
       <Button variant="contained">Entrar</Button>
+
     </Box>
   );
 }
