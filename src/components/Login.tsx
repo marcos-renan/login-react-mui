e015a8c001bce7cdcf759216f3c9ee03885b9005
Marcos <devmarcos7@gmail.com>
2025-03-31 09:48:35 -0300

    reorganizando projeto

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -5,25 +5,14 @@ const Login = () => {
 
   return (
     
-    <Box
-      bgcolor={"#7159c1"}
-      display={"flex"}
-      flexDirection={"column"}
-      minHeight={"100vh"}
-    >
+    <Box bgcolor={"#7159c1"} display={"flex"} flexDirection={"column"} minHeight={"100vh"}>
 
       <Typography align="center">Acessar a conta</Typography>
 
-      <Box
-        component={"form"}
-        bgcolor={"#d7d2d7"}
-        display={"flex"}
-        flexDirection={"column"}
-      >
+      <Box component={"form"} bgcolor={"#d7d2d7"} display={"flex"} flexDirection={"column"}>
         <FormControl>
           <TextField type="email" label="E-mail" variant="outlined" />
         </FormControl>
-
         <TextField type="password" label="Senha" variant="outlined" />
 
       </Box>
