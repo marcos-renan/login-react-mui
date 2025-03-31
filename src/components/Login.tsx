ca30af833e7622778b515cd64c8415dc9d36069e
Marcos <devmarcos7@gmail.com>
2025-03-31 10:43:27 -0300

    reformulando formulario

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,26 +1,31 @@
-import { Box, Button, FormControl, TextField, Typography } from "@mui/material"
+import { CheckBox } from "@mui/icons-material";
+import { Box, Button, FormControl, FormControlLabel, TextField, Typography } from "@mui/material";
 
 
 const Login = () => {
 
   return (
-    
-    <Box bgcolor={"#7159c1"} display={"flex"} flexDirection={"column"} minHeight={"100vh"}>
-
-      <Typography align="center">Acessar a conta</Typography>
-
-      <Box component={"form"} bgcolor={"#d7d2d7"} display={"flex"} flexDirection={"column"}>
-        <FormControl>
-          <TextField type="email" label="E-mail" variant="outlined" />
-        </FormControl>
-        <TextField type="password" label="Senha" variant="outlined" />
-
+    <>
+      {/* container principal */} {/* */}
+      <Box bgcolor='green' display='flex' flexDirection='column' minHeight='100vh' component='form'>
+        <Typography variant="h3" align="center"> Login</Typography>
+        {/* container do formulario */}
+        <Box bgcolor='white' display='flex' flexDirection='column'>
+          {/* formulario */}
+          <FormControl>
+            {/* input do email*/}
+            <TextField type="email" placeholder="E-mail" variant="outlined" required/>
+            {/* input da senha */}
+            <TextField type="password" placeholder="E-Senha" variant="outlined" required/>
+            {/* checkbox de relembrar dados*/}
+            <FormControlLabel control={<CheckBox value="remember" color="primary" required/>} label="Relembre-me" />
+            {/* botao de entrar*/}
+            <Button type="submit" variant="contained" color="primary">Entrar</Button>
+          </FormControl>
+        </Box>
       </Box>
-
-      <Button variant="contained">Entrar</Button>
-
-    </Box>
+    </>
   );
 }
 
-export default Login
+export default Login;
