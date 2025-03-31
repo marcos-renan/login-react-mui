7494881a4733bf8e1db46d97042038c846f83b45
Marcos <devmarcos7@gmail.com>
2025-03-31 15:07:17 -0300

    entrar com sociais

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,4 +1,5 @@
 import { Box, Button, FormControl, Link, TextField, Typography } from "@mui/material";
+import { Google, Facebook } from "@mui/icons-material";
 
 const Login = () => {
 
@@ -20,13 +21,20 @@ const Login = () => {
           <Button type="submit" variant="contained" color="primary">Entrar</Button>
         </Box>
         {/* links de 'esqueci senha' e 'cadastro' */}
-        <Box width="100%" display="flex" justifyContent="space-between" maxWidth="360px">
+        <Box width="100%" display="flex" justifyContent="space-between" maxWidth="360px" alignItems="center" mt={1}>
           <Link href="#" variant="body2">
             Esqueci minha senha
           </Link>
           <Link href="#" variant="body2" >
             Cadastrar-se
           </Link>
+        </Box>
+        {/* botoes para entrar com google ou facebook */}
+        <Box display="flex" flexDirection="column" gap={2} mt={2}>
+          {/* botao google */}
+          <Button variant="outlined" startIcon={<Google/>}>Entrar com Google</Button>
+          {/* botao facebook */}
+          <Button variant="outlined" startIcon={<Facebook/>}>Entrar com Facebook</Button>
         </Box>
       </Box>
     </>
